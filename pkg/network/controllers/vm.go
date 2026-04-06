ac76b28428d4381720c6134fbc3fb5e1fc65809f
zbb88888 <jmdxjsjgcxy@gmail.com>
2026-04-06 17:46:41 +0800

    live update qos
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- pkg/network/controllers/vm.go
+++ pkg/network/controllers/vm.go
@@ -174,15 +174,22 @@ func applyDynamicIfaceRequestOnVMI(
 			vmIface.State != vmiIfaceCopy.State &&
 			vmiIfaceCopy.State != v1.InterfaceStateAbsent
 
+		shouldUpdateExistingIfaceBandwidth := existsInVMISpec &&
+			!equality.Semantic.DeepEqual(vmIface.Bandwidth, vmiIfaceCopy.Bandwidth) &&
+			vmiIfaceCopy.State != v1.InterfaceStateAbsent
+
 		switch {
 		case shouldHotplugIface:
 			vmiSpecCopy.Networks = append(vmiSpecCopy.Networks, vmIndexedNetworks[vmIface.Name])
 			vmiSpecCopy.Domain.Devices.Interfaces = append(vmiSpecCopy.Domain.Devices.Interfaces, vmIface)
 
-		case shouldUpdateExistingIfaceState:
+		case shouldUpdateExistingIfaceState || shouldUpdateExistingIfaceBandwidth:
 			if !(hasOrdinalIfaces && vmIface.State == v1.InterfaceStateAbsent) {
 				vmiIface := vmispec.LookupInterfaceByName(vmiSpecCopy.Domain.Devices.Interfaces, vmIface.Name)
 				vmiIface.State = vmIface.State
+				if shouldUpdateExistingIfaceBandwidth {
+					vmiIface.Bandwidth = vmIface.Bandwidth.DeepCopy()
+				}
 			}
 		}
 	}
--- pkg/virt-launcher/virtwrap/network/nichotplug.go
+++ pkg/virt-launcher/virtwrap/network/nichotplug.go
@@ -25,6 +25,8 @@ import (
 	"fmt"
 	"strings"
 
+	"k8s.io/apimachinery/pkg/api/equality"
+
 	"libvirt.org/go/libvirt"
 
 	v1 "kubevirt.io/api/core/v1"
@@ -102,8 +104,18 @@ func (vim *virtIOInterfaceManager) updateDomainLinkState(currentDomain, desiredD
 			continue
 		}
 
+		changed := false
 		if !isLinkStateEqual(curIface, desiredIface) {
 			curIface.LinkState = desiredIface.LinkState
+			changed = true
+		}
+
+		if !equality.Semantic.DeepEqual(curIface.BandWidth, desiredIface.BandWidth) {
+			curIface.BandWidth = desiredIface.BandWidth
+			changed = true
+		}
+
+		if changed {
 			if err := vim.updateIfaceInDomain(&curIface); err != nil {
 				return err
 			}
